d93c07583d03e6c1ff6352e0ee9b10776047b21f
Eli <52673344+elibullockpapa@users.noreply.github.com>
2024-10-18 23:42:12 -0400

    added clerk package, middleware, and updated spacing for eslint prettier

--- app/providers.tsx
+++ app/providers.tsx
@@ -5,18 +5,23 @@ import { NextUIProvider } from "@nextui-org/system";
 import { useRouter } from "next/navigation";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ThemeProviderProps } from "next-themes/dist/types";
+import { ClerkProvider } from "@clerk/nextjs";
 
 export interface ProvidersProps {
-  children: React.ReactNode;
-  themeProps?: ThemeProviderProps;
+    children: React.ReactNode;
+    themeProps?: ThemeProviderProps;
 }
 
 export function Providers({ children, themeProps }: ProvidersProps) {
-  const router = useRouter();
+    const router = useRouter();
 
-  return (
-    <NextUIProvider navigate={router.push}>
-      <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
-    </NextUIProvider>
-  );
+    return (
+        <ClerkProvider>
+            <NextUIProvider navigate={router.push}>
+                <NextThemesProvider {...themeProps}>
+                    {children}
+                </NextThemesProvider>
+            </NextUIProvider>
+        </ClerkProvider>
+    );
 }
