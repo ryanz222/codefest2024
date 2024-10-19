1b60764df19ef1dcaa66325506b805e7601d560c
Eli <52673344+elibullockpapa@users.noreply.github.com>
2024-10-18 23:47:17 -0400

    remove eslint warning, clean up navbar and icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,31 @@
 import "@/styles/globals.css";
-import { Metadata, Viewport } from "next";
-import { Link } from "@nextui-org/link";
 import clsx from "clsx";
 
 import { Providers } from "./providers";
 
-import { siteConfig } from "@/config/site";
 import { fontSans } from "@/config/fonts";
 import { Navbar } from "@/components/navbar";
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-    <html suppressHydrationWarning lang="en">
-      <head />
-      <body
-        className={clsx(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable,
-        )}
-      >
-        <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
-          <Navbar />
-          <main>{children}</main>
-        </Providers>
-      </body>
-    </html>
-  );
+    return (
+        <html suppressHydrationWarning lang="en">
+            <head />
+            <body
+                className={clsx(
+                    "min-h-screen bg-background font-sans antialiased",
+                    fontSans.variable,
+                )}
+            >
+                <Providers
+                    themeProps={{ attribute: "class", defaultTheme: "light" }}
+                >
+                    <Navbar />
+                    <main>{children}</main>
+                </Providers>
+            </body>
+        </html>
+    );
 }
